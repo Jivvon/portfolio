7be16b5a5f779460e6d425786f64209b5092344e
Jeong Ji Won <poiu8944@gmail.com>
2021-02-02 02:43:49 +0900

    Updates

---
+++ precache-manifest.524de22adc6d2fd9b0b50679cff48e69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8183e24d08669d8a205",
+    "url": "/portfolio/static/css/main.7c5018df.chunk.css"
+  },
+  {
+    "revision": "f8183e24d08669d8a205",
+    "url": "/portfolio/static/js/main.f8183e24.chunk.js"
+  },
+  {
+    "revision": "738dafd78164cb4fc829",
+    "url": "/portfolio/static/css/1.721f853a.chunk.css"
+  },
+  {
+    "revision": "738dafd78164cb4fc829",
+    "url": "/portfolio/static/js/1.738dafd7.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "16c61b672cc9d65d00bd8cb3391da0cc",
+    "url": "/portfolio/index.html"
+  }
+];
